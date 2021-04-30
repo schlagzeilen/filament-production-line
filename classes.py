12b5736c043f97e1bd7c8bc26975cb511e5c88fd
schlagzeilen <tibor3696@googlemail.com>
2021-04-30 20:36:05 +0200

    sicherheitshalber

--- classes.py
+++ classes.py
@@ -2,7 +2,6 @@ from pthat.pthat import Axis
 from gpiozero import Button
 import datetime
 
-print("basinga")
 class rot_axis(Axis):
     pulling_speed_ist= float()
     pulling_speed_soll= float()
@@ -44,11 +43,7 @@ class maschine():
     buffer_hight_ist =float()
     buffer_hight_soll = float()
 
-    switches = [switch(1), switch(2),switch(3)]
 
-    abzug =         rot_axis("X",command_id=1, serial_device="/dev/ttyS0")
-    wicklung  =     rot_axis("Z",command_id=2, serial_device="/dev/ttyS0")
-    y_axis =        lin_axis("Y",command_id=3, serial_device="/dev/ttyS0")
 
     #--------------------getters
     #--------------------setters
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-import classes
+from classes import rot_axis,lin_axis,maschine
+from interface import *
+mmaschine = maschine()
+
+switches = [switch(1), switch(2),switch(3)]
+
+abzug =         rot_axis("X",command_id=1, serial_device="/dev/ttyS0")
+wicklung  =     rot_axis("Z",command_id=2, serial_device="/dev/ttyS0")
+y_axis =        lin_axis("Y",command_id=3, serial_device="/dev/ttyS0")
